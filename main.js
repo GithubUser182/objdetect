9058601c18e5094e665594e9ba6840a037a4ca83
GithubUser182 <112442002+GithubUser182@users.noreply.github.com>
2023-06-21 22:47:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function mug(){
+    window.location.replace("mug.html")
+}
+
+function cat(){
+    window.location.replace("cat.html")
+}
+
+function hat(){
+    window.location.replace("xboxhat.html")
+}
+
+function bed(){
+    window.location.replace("bedroom.html")
+}
+
+function clouds(){
+    window.location.replace("clouds.html")
+}
